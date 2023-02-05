0f15059bcb99d7c1eca9596134965d67dfd811fe
HarryUnderscore13 <118264484+HarryUnderscore13@users.noreply.github.com>
2023-02-05 20:50:31 +0000

    API now returns response from lat & lon corordinates, got needed data as variables and display todays weather in main section. Clears previous data on new search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,63 @@
-$("#search-button").on("click", function(event){
-    event.preventDefault();
-    let location = $("#search-input").val();
+let todayEl = $("#today"); // Link todayEl to html section
 
-//    console.log(location); 
 
-    let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + location +
-    "&limit=5&appid=6090686048651d85250d11bb39ac022a";
-    
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+$("#search-button").on("click", function (event) { // when search button is clicked, run function
+  
+    $("#today").empty();
+
+    event.preventDefault(); 
+// -----------Display Location Searched and Todays Date--------------------------------------
+  let location = $("#search-input").val(); // create variable with user search query
+  let currentCityAndDate = $("<h2>"); // create a new H2 element
+  currentCityAndDate.text(location + " " + "(" + moment().format('L') + ")"); // Add text of user search to new H2 & date
+  todayEl.append(currentCityAndDate); // display current city & date on page
       
-        .then(function(response) {
-        
-// console.log(response);
 
-        let lat = response[0].lat;
-        let lon = response[0].lon;
+  let queryURL = // URL to send to the API including key and searched city
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    location +
+    "&limit=5&appid=6090686048651d85250d11bb39ac022a";
 
-        let weatherURL = "api.openweathermap.org/data/2.5/forecast?lat="
-         + lat + "&lon=" + lon +
-        "&appid=6090686048651d85250d11bb39ac022a";  
-        
-// console.log(weatherURL);
+  $.ajax({ // Get info from API
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) { // Once data is returned run function
 
-        // $.ajax({
-        //     url: weatherURL,
-        //     method: "GET"
-        // }) .then(function(getWeather){
+    let lat = response[0].lat; // create variable for city lat
+    let lon = response[0].lon; // create variable for city lon
 
-        //     console.log(getWeather);
+    getWeather(lat, lon); // run function using lat and lon as parameters
 
-        // })
+});
 
-   
-        })
-    })
+    function getWeather(lat, lon) { // function to get weather data using 
+      let weatherURL =
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" + // create URL for API using lat and lon for users city search
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=6090686048651d85250d11bb39ac022a";
+
+      $.ajax({ // Send URl to API to get weather data 
+        url: weatherURL,
+        method: "get",
+      }).then(function (response) {
+        // console.log(response);
+
+        let temp = $("<h4>");
+        temp.text(response.list[0].main.temp);
+        todayEl.append(temp);
+        let wind = $("<h4>");
+        wind.text(response.list[0].wind.speed);
+        todayEl.append(wind);
+        let humidity = $("<h4>");
+        humidity.text(response.list[0].main.humidity);
+        todayEl.append(humidity);
+
+
+
+    });
+
+
+    }
+});
